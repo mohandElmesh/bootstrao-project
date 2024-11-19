34d3854c8095a3da99c82b7d86184a50cd2840da
mohandElmesh <mohand.elmesh74@gmail.com>
2024-11-19 18:17:54 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var alertbtn=document.querySelector(".alert1")
+
+function clickalert(){
+  alert("تم الضغط")
+
+}
+
+alertbtn.onclick= clickalert
+
+
+var alertbtn2=document.querySelector(".alert2")
+
+function clickalert2(){
+  alert("تم الضغط")
+
+}
+
+alertbtn2.onclick= clickalert2
+
+
+
+var alertbtn3=document.querySelector(".alert3")
+
+function clickalert3(){
+  alert("تم الضغط")
+
+}
+
+alertbtn3.onclick= clickalert3
